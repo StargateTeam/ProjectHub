0149754c9f9318efffea46e07307885507577a3f
UNNEW-main <1970005826c@gmail.com>
2022-02-18 22:43:29 +0900

    feature: Login Page UI 구현

---
+++ pages/Login.tsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Login() {
+  return (
+    <Container>
+      <LogoWrapper>
+        <Logo src="https://cdn-icons-png.flaticon.com/512/6877/6877758.png"></Logo>
+      </LogoWrapper>
+      <TitleWrapper>Sign in to 면접프로젝트이름</TitleWrapper>
+      <LoginFormContainer>
+        <InputWrapper>
+          <EmailaddressText>Email address</EmailaddressText>
+          <EmailaddressInput></EmailaddressInput>
+          <PasswordTextWrapper>
+            <PasswordText>Password</PasswordText>
+            <PasswordForgotText>비밀번호 찾기</PasswordForgotText>
+          </PasswordTextWrapper>
+
+          <PasswordInput></PasswordInput>
+        </InputWrapper>
+
+        <SubmitButton>Sign in</SubmitButton>
+      </LoginFormContainer>
+      <SignUpWrapper>
+        <SignUpText>계정이 없으신가요?</SignUpText>
+        <SignUpButton>계정 만들기</SignUpButton>
+      </SignUpWrapper>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  /* background-color: red; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+`;
+const LogoWrapper = styled.div``;
+const Logo = styled.img`
+  width: 80px;
+  height: 80px;
+`;
+const TitleWrapper = styled.div`
+  margin: 15px 0px;
+  font-size: 20px;
+`;
+
+const LoginFormContainer = styled.form`
+  width: 300px;
+  height: 240px;
+  background-color: #f9f9f9;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  padding: 20px 20px;
+`;
+
+const InputWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+const EmailaddressText = styled.div`
+  width: 100%;
+  margin-bottom: 10px;
+`;
+const EmailaddressInput = styled.input`
+  width: 100%;
+  margin-bottom: 20px;
+  height: 30px;
+  border-radius: 5px;
+  border: none;
+`;
+
+const PasswordTextWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  width: 100%;
+  margin-bottom: 10px;
+`;
+const PasswordText = styled.div``;
+const PasswordForgotText = styled.div`
+  color: #1480ff;
+  font-size: 10px;
+  cursor: pointer;
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+const PasswordInput = styled.input`
+  width: 100%;
+  height: 30px;
+  border-radius: 5px;
+
+  border: none;
+`;
+
+const SubmitButton = styled.div`
+  width: auto;
+  padding: 8px 35px;
+  color: #ffffff;
+  background-color: #40d269;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+const SignUpWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+`;
+const SignUpText = styled.div``;
+const SignUpButton = styled.div``;
