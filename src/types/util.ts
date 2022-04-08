6b20c12b401540fd341026c2759fff832adb853c
KZ-Lucas <qq3884qq@naver.com>
2022-04-08 17:29:25 +0900

    feature: 컬러 시스템 추가

--- src/types/util.ts
+++ src/types/util.ts
@@ -1,3 +1,11 @@
+/* eslint no-use-before-define: 0 */ // --> OFF
+// @ts-ignore
+type Join<K, P> = K extends string | number
+  ? P extends string | number
+    ? `${K}${'' extends P ? '' : '.'}${P}`
+    : never
+  : never
+
 export declare type ResponsiveValue<T> =
   | T
   | null
@@ -6,3 +14,7 @@ export declare type ResponsiveValue<T> =
   | {
       [key in string | number]?: T
     }
+
+export type Leaves<T> = T extends object
+  ? { [K in keyof T]-?: Join<K, Leaves<T[K]>> }[keyof T]
+  : ''
