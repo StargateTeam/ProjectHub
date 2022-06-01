b2d90f5495a122ac645dae276275f196b14a7e38
JeongYunMi <50102538+JeongYunMi@users.noreply.github.com>
2022-06-02 04:40:31 +0900

    Update BannersSlideItem.style.tsx
    error 코드

--- src/components/Oraganisms/Headers/BannersSlideItem.style.tsx
+++ src/components/Oraganisms/Headers/BannersSlideItem.style.tsx
@@ -12,38 +12,27 @@ export const BannersItemWrap = styled.div`
   position: relative;
 `
 
-export const OverlayImage = styled.div`
-  background-color: #61616147;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  z-index: -1;
-`
-
-export const BannerBgImg = styled.picture`
+export const BannerBgSVGWrap = styled.div`
   position: absolute;
   width: 100%;
   height: 100%;
   z-index: -2;
 `
 
-export const SourceImage = styled.source`
-  width: 100vw;
-  object-fit: cover;
-`
+export const BlurSVG = styled.svg``
 
-export const DefaulImage = styled.img`
-  width: 100vw;
-  object-fit: cover;
-`
+export const BlurDefs = styled.defs``
+
+export const BlurFilter = styled.filter``
+
+export const BlurFeGaussianBlur = styled.feGaussianBlur``
+
+export const BannerBgImg = styled.image``
 
 export const ZoomInBannerImg = styled.img`
   width: 368px;
   height: 248px;
-  background-size: cover;
-  filter: blur(0px);
   border: 1px solid silver;
-  border-radius: 4px;
 `
 
 export const UserTextWrap = styled(Box)``
